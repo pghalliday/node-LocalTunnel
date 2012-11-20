47f835e65aa2c76235bdc191d3b445056ce5288a
Peter Halliday <pghalliday@gmail.com>
2012-11-20 14:23:01 +0100

    integrating with new ChildDaemon package

--- src/LocalTunnel.js
+++ src/LocalTunnel.js
@@ -1,15 +1,27 @@
 var util = require('util'),
     ChildDaemon = require('child-daemon');
 
-function LocalTunnel(port, binary) {
-  binary = binary || 'localtunnel';
-  ChildDaemon.call(
-    this,
-    binary,
-    [port],
-    new RegExp('[a-z0-9]{4}\\.localtunnel\\.com')
-  );
-  
+function LocalTunnel(port) {
+  var match = new RegExp('[a-z0-9]{4}\\.localtunnel\\.com');
+
+  if (process.platform === 'win32') {
+    // on windows we need to call ruby and search the path for the localtunnel program
+    // as child_process.spawn (used by ChildDaemon) only supports .exe files
+    ChildDaemon.call(
+      this,
+      'ruby',
+      ['-S', 'localtunnel', port],
+      match
+    );    
+  } else {
+    ChildDaemon.call(
+      this,
+      'localtunnel',
+      [port],
+      match
+    );    
+  }
+
   this.superStart = this.start;
   this.start = function(callback) {
     this.superStart(function(error, matched) {
